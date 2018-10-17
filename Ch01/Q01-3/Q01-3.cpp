2a9571362fca66e44231ae42f2661c7ebd6499ef
혜진 김 <traceimp@outlook.kr>
2018-10-17 14:22:23 +0900

    DefaultValue2 수정, Q01-3 주석 해제

--- Ch01/Q01-3/Q01-3.cpp
+++ Ch01/Q01-3/Q01-3.cpp
@@ -7,33 +7,33 @@
 	실행결과도 동일해야 한다.
 */
 
-//int BoxVolume(int length, int width, int height);
-//int BoxVolume(int length, int width);
-//int BoxVolume(int length);
-//
-//int main(void)
-//{
-//	std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl;
-//	std::cout << "[5, 5, D] : " << BoxVolume(5, 5) << std::endl;
-//	std::cout << "[7, D, D] : " << BoxVolume(7) << std::endl;
-//	//std::cout << "[D, D, D] : " << BoxVolume() << std::endl;
-//	return 0;
-//}
-//
-//int BoxVolume(int length, int width, int height)
-//{
-//	return length * width*height;
-//}
-//
-//int BoxVolume(int length, int width)
-//{
-//	return length * width*1;
-//}
-//
-//int BoxVolume(int length)
-//{
-//	return length*1*1;
-//}
+int BoxVolume(int length, int width, int height);
+int BoxVolume(int length, int width);
+int BoxVolume(int length);
+
+int main(void)
+{
+	std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl;
+	std::cout << "[5, 5, D] : " << BoxVolume(5, 5) << std::endl;
+	std::cout << "[7, D, D] : " << BoxVolume(7) << std::endl;
+	//std::cout << "[D, D, D] : " << BoxVolume() << std::endl;
+	return 0;
+}
+
+int BoxVolume(int length, int width, int height)
+{
+	return length * width*height;
+}
+
+int BoxVolume(int length, int width)
+{
+	return length * width*1;
+}
+
+int BoxVolume(int length)
+{
+	return length*1*1;
+}
 
 /*
 	문제2
