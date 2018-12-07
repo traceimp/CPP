0bb39e10b0911184864a999b372b7590f06d1064
traceimp <traceimp@outlook.kr>
2018-12-07 16:15:21 +0900

    파트3 > 챕터 9 완료

---
+++ Ch09/MultiInheri1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class BaseOne
+{
+  public:
+    void SimpleFuncOne() { cout << "BaseOne" << endl; }
+};
+
+class BaseTwo
+{
+  public:
+    void SimpleFuncTwo() { cout << "BaseTwo" << endl; }
+};
+
+class MultiDerived : public BaseOne, protected BaseTwo
+{
+  public:
+    void ComplexFunc()
+    {
+        SimpleFuncOne();
+        SimpleFuncTwo();
+    }
+};
+
+int main(void)
+{
+    MultiDerived mdr;
+    mdr.ComplexFunc();
+
+    return 0;
+}
---
+++ Ch09/MultiInheri3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Base
+{
+  public:
+    Base() { cout << "Base Constructor" << endl; }
+    void SimpleFunc() { cout << "BaseOne" << endl; }
+};
+
+class MiddleDerivedOne : virtual public Base
+{
+  public:
+    MiddleDerivedOne() : Base()
+    {
+        cout << "MiddleDerivedOne Constructor" << endl;
+    }
+
+    void MiddleFuncOne()
+    {
+        SimpleFunc();
+        cout << "MiddleDerivedOne" << endl;
+    }
+};
+
+class MiddleDerivedTwo : virtual public Base
+{
+  public:
+    MiddleDerivedTwo() : Base()
+    {
+        cout << "MiddleDerivedTwo Constructor" << endl;
+    }
+    void MiddleFuncTwo()
+    {
+        SimpleFunc();
+        cout << "MiddleDerivedTwo" << endl;
+    }
+};
+
+class LastDerived : public MiddleDerivedOne, public MiddleDerivedTwo
+{
+  public:
+    LastDerived() : MiddleDerivedOne(), MiddleDerivedTwo()
+    {
+        cout << "LastDerived Comstructor" << endl;
+    }
+
+    void ComplexFunc()
+    {
+        MiddleFuncOne();
+        MiddleFuncTwo();
+        SimpleFunc();
+    }
+};
+
+int main(void)
+{
+    cout << "객체생성 전....." << endl;
+    LastDerived ldr;
+    cout << "객체생성 후....." << endl;
+    ldr.ComplexFunc();
+
+    return 0;
+}
